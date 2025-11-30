be6d75778bf11a0b74af71c626eece637aa87541
RJ <randallhunter2002@gmail.com>
2025-11-30 20:19:11 -0330

    Module to store questions

--- questions.js
+++ questions.js
@@ -21,4 +21,133 @@ const questions = {
         ]
     },
     
+    3: {
+        "Question": "What is the maximum number of friends a player can have on Roblox?",
+        "Correct": "200",
+        "Incorrect": [
+            "50",
+            "100",
+            "Unlimited"
+        ]
+    },
+
+    4: {
+        "Question": "What is the name of Roblox's physics engine?",
+        "Correct": "PBD (Position-Based Dynamics)",
+        "Incorrect": [
+            "Nexus Engine",
+            "RBLX Engine",
+            "Kinetic Builder"
+        ]
+    },
+
+    5: {
+        "Question": "What is the name of Roblox's physics engine?",
+        "Correct": "PBD (Position-Based Dynamics)",
+        "Incorrect": [
+            "Nexus Engine",
+            "RBLX Engine",
+            "Kinetic Builder"
+        ]
+    },
+
+    6: {
+        "Question": "What is the name of Roblox's physics engine?",
+        "Correct": "PBD (Position-Based Dynamics)",
+        "Incorrect": [
+            "Nexus Engine",
+            "RBLX Engine",
+            "Kinetic Builder"
+        ]
+    },
+
+    7: {
+        "Question": "What is the name of Roblox's physics engine?",
+        "Correct": "PBD (Position-Based Dynamics)",
+        "Incorrect": [
+            "Nexus Engine",
+            "RBLX Engine",
+            "Kinetic Builder"
+        ]
+    },
+
+    8: {
+        "Question": "What is the name of Roblox's physics engine?",
+        "Correct": "PBD (Position-Based Dynamics)",
+        "Incorrect": [
+            "Nexus Engine",
+            "RBLX Engine",
+            "Kinetic Builder"
+        ]
+    },
+
+    9: {
+        "Question": "What is the name of Roblox's physics engine?",
+        "Correct": "PBD (Position-Based Dynamics)",
+        "Incorrect": [
+            "Nexus Engine",
+            "RBLX Engine",
+            "Kinetic Builder"
+        ]
+    },
+
+    10: {
+        "Question": "What is the name of Roblox's physics engine?",
+        "Correct": "PBD (Position-Based Dynamics)",
+        "Incorrect": [
+            "Nexus Engine",
+            "RBLX Engine",
+            "Kinetic Builder"
+        ]
+    },
+
+    11: {
+        "Question": "What is the name of Roblox's physics engine?",
+        "Correct": "PBD (Position-Based Dynamics)",
+        "Incorrect": [
+            "Nexus Engine",
+            "RBLX Engine",
+            "Kinetic Builder"
+        ]
+    },
+
+    12: {
+        "Question": "What is the name of Roblox's physics engine?",
+        "Correct": "PBD (Position-Based Dynamics)",
+        "Incorrect": [
+            "Nexus Engine",
+            "RBLX Engine",
+            "Kinetic Builder"
+        ]
+    },
+
+    13: {
+        "Question": "What is the name of Roblox's physics engine?",
+        "Correct": "PBD (Position-Based Dynamics)",
+        "Incorrect": [
+            "Nexus Engine",
+            "RBLX Engine",
+            "Kinetic Builder"
+        ]
+    },
+
+    14: {
+        "Question": "What is the name of Roblox's physics engine?",
+        "Correct": "PBD (Position-Based Dynamics)",
+        "Incorrect": [
+            "Nexus Engine",
+            "RBLX Engine",
+            "Kinetic Builder"
+        ]
+    },
+
+    15: {
+        "Question": "What is the name of Roblox's physics engine?",
+        "Correct": "PBD (Position-Based Dynamics)",
+        "Incorrect": [
+            "Nexus Engine",
+            "RBLX Engine",
+            "Kinetic Builder"
+        ]
+    },
 };
