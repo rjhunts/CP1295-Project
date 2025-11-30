c875c93c8414f7f86ae28b6c271afd89266ab245
RJ <randallhunter2002@gmail.com>
2025-11-30 19:35:35 -0330

    Module to store questions

---
+++ questions.js
@@ -0,0 +1,24 @@
+export {questions};
+
+const questions = {
+    1: {
+        "Question": "What year was Roblox officially released to the public?",
+        "Correct": "2006",
+        "Incorrect": [
+            "2004",
+            "2006",
+            "2010"
+        ]
+    },
+    
+    2: {
+        "Question": "What is the Roblox in-game currency called?",
+        "Correct": "Robux",
+        "Incorrect": [
+            "Robux Coins",
+            "R-Bucks",
+            "Roblox Tokens"
+        ]
+    },
+    
+};
