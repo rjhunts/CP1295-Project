a7dae3a94a541f8317defde44a4570cdd0bfd400
RJ <randallhunter2002@gmail.com>
2025-12-01 02:27:43 -0330

    Removed a lot of the functionality due to intervals not working correctly

--- game.js
+++ game.js
@@ -15,70 +15,40 @@ function getQuestions() {
     }
 
     return random_questions;
-}
+};
 
-function game(question) {
-    const answers = question.Incorrect;
-    const randomIndex = Math.floor(Math.random() * 4);
-
-    answers.splice(randomIndex, 0, question.Correct);
-    
-    const question_title = $("question-title");
-    question_title.textContent = question.Question;
-
-    const buttons = $("answer-buttons");
-
-    let i = 0;
-    for (let button of buttons.children) {
-        button.classList.remove("selected")
-        button.textContent = answers[i];
-        i++;
+let timerInterval = null;
+let counter = 0;
+function askQuestion() {
+    counter++;
+    if (parseInt(counter) > 10) {
+        return;
     }
-
-    let timer = 15
-    $("timer").textContent = `Timer: ${timer}`;
-
-    const intervalId = setInterval(() => {
-        timer--
-        console.log(timer);
-        $("timer").textContent = `Timer: ${timer}`;
-        if (timer <= 0) {
-            clearInterval(intervalId);
+    console.log(counter)
+    
+    clearInterval(timerInterval);
+    const displayTimer = $("timer");
+    displayTimer.textContent = "Timer: 15";
+    let timeCount = 15;
+    timerInterval = setInterval(() => {
+        timeCount--;
+        displayTimer.textContent = `Timer: ${timeCount}`
+        if (timeCount <= 0) {
+            clearInterval(timerInterval);
         }
-    }, 1000);
-
-    const question_form = $("question-form");
-    question_form.addEventListener("submit", (evt) => {
-        evt.preventDefault();
-
-        const selected = $("selected-answer");
-        clearInterval(intervalId);
-        return selected.value;
-    });
-} 
+    }, 1000)
+};
 
 function main() {
 
-    let questions = getQuestions();
-    let count = 1
-    console.log(count)
-    
-    let correctAnswers = 0
+    const random_questions = getQuestions();
+    const question_form = $("question-form");
+    const answers = [];
 
-    let question = questions.pop(0);
-    if (game(question) == question.Correct) {
-        correctAnswers++
-    }
+    askQuestion();
 
-    const intervalId = setInterval(() => {
-        count++
-        console.log(count)
-        question = questions.pop(0);
-        if (game(question) == question.Correct) {
-            correctAnswers++
-        }
-        if (count >= 15) {
-            clearInterval(intervalId);
-        }
-    }, 16000);
+    question_form.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+        askQuestion();
+    })
 };
